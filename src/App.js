e9d4b9c2ebdc766b25c0a5d7c59c30cff2f4a7e0
MSzostaq <maciek.szostak55@gmail.com>
2021-10-11 18:12:16 +0200

    code refactoring

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import ModalsProvider from "components/ModalsProvider";
 import Modals from "components/Modals";
@@ -16,10 +16,6 @@ const AppNotifications = styled(Notifications)`
 `;
 
 const App = () => {
-  useEffect(() => {
-    document.title = "Tic-Tac-Toe";
-  });
-
   return (
     <StyledApp>
       <ModalsProvider>
