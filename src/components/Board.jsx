135053fdfd46bfec433c4263dc89d8ccd847c6c0
MSzostaq <maciek.szostak55@gmail.com>
2021-10-21 14:31:59 +0200

    changed board styles

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,11 +3,13 @@ import styled from "styled-components";
 import Square from "components/Square";
 
 const Wrapper = styled.div`
+  background-color: ${({ theme }) => theme.colors.darkGrey};
   border-radius: 8px;
   box-shadow: 0px 0px 2px 0px ${({ theme }) => theme.colors.darkPurple};
   display: grid;
-  grid-template: repeat(3, 1fr) / repeat(3 / 1fr);
-  gap: 1rem;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  gap: 0.3rem;
   width: 500px;
   height: 500px;
 `;
