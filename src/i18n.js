2fdcf99487f968131cfcc03db4f3d4b57bf2b2e9
MSzostaq <maciek.szostak55@gmail.com>
2021-11-19 15:52:58 +0100

    added i18n file

---
+++ src/i18n.js
@@ -0,0 +1,24 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import translationEN from "translations/en/translationEN.json";
+import translationPL from "translations/pl/translationPL.json";
+
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  pl: {
+    translation: translationPL,
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "en",
+  keySeparator: false,
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
