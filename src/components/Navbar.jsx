bef484b26028a20da33d0d88e38056ef0bd5f4e3
MSzostaq <maciek.szostak55@gmail.com>
2021-11-19 11:01:52 +0100

    added Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { useTranslation } from "react-i18next";
+import LanguageToggle from "components/LanguageToggle";
+
+const StyledNavbar = styled.div`
+  background-color: ${({ theme }) => theme.colors.lightGrey};
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 56px;
+`;
+
+const Navbar = () => {
+  const { i18n } = useTranslation();
+
+  const [value, setValue] = useState(false);
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
+
+  const onValueChange = (newValue) => {
+    setValue(newValue);
+    changeLanguage(newValue ? "pl" : "en");
+  };
+
+  return (
+    <StyledNavbar>
+      <LanguageToggle value={value} onChange={onValueChange} />
+    </StyledNavbar>
+  );
+};
+
+export default Navbar;
