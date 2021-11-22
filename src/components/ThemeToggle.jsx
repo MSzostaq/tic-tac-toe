bc1a758d5405c35111fe55e04bcf616f877948c6
MSzostaq <maciek.szostak55@gmail.com>
2021-11-22 12:59:38 +0100

    added ThemeToggle component

---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styled from "styled-components";
+import moon from "assets/moon.png";
+import sun from "assets/sun.png";
+
+const ToggleButton = styled.button`
+  background-color: ${({ theme }) => theme.colors.darkPurple};
+  border-radius: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: ${({ isOn }) => (isOn ? "flex-end" : "flex-start")};
+  margin-right: 12px;
+  width: 56px;
+  height: 32px;
+
+  @media (min-width: 1800px) {
+    border-radius: 24px;
+    width: 80px;
+    height: 48px;
+  }
+`;
+
+const Switch = styled.div`
+  background-color: ${({ theme }) => theme.colors.background};
+  border-radius: 50%;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 32px;
+  height: 32px;
+
+  @media (min-width: 1800px) {
+    width: 48px;
+    height: 48px;
+  }
+`;
+
+const SwitchImage = styled.div`
+  background: url(${({ isOn }) => (isOn ? sun : moon)});
+  background-color: ${({ theme }) => theme.colors.background};
+  background-repeat: no-repeat;
+  background-size: contain;
+  border-radius: 50%;
+  width: 28px;
+  height: 28px;
+`;
+
+const ThemeToggle = ({ value, onChange }) => {
+  function onLanguageToggleClick() {
+    onChange(!value);
+  }
+
+  return (
+    <ToggleButton isOn={value} onClick={onLanguageToggleClick}>
+      <Switch isOn={value}>
+        <SwitchImage />
+      </Switch>
+    </ToggleButton>
+  );
+};
+
+export default ThemeToggle;
