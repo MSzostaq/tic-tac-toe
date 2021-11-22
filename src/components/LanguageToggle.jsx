e396c370637bd3c3d220290462685c219fcaa026
MSzostaq <maciek.szostak55@gmail.com>
2021-11-22 13:00:02 +0100

    changed LanguageToggle background-color

--- src/components/LanguageToggle.jsx
+++ src/components/LanguageToggle.jsx
@@ -4,7 +4,7 @@ import pl_circle from "assets/pl_circle.png";
 import uk_circle from "assets/uk_circle.png";
 
 const ToggleButton = styled.button`
-  background-color: #696969;
+  background-color: ${({ theme }) => theme.colors.darkPurple};
   border-radius: 16px;
   display: flex;
   align-items: center;
