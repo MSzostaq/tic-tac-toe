10556fe43e0e4878f513525fe2ae4f28950ab907
MSzostaq <maciek.szostak55@gmail.com>
2021-11-23 10:00:14 +0100

    added theme.js file

---
+++ src/themes/theme.js
@@ -0,0 +1,47 @@
+export const lightTheme = {
+  colors: {
+    background: "#f5f5f5",
+    black: "#222222",
+    blue: "#0f5298",
+    board: "#f9f9f9",
+    darkPurple: "#acb3c0",
+    error: "#ce5a5a",
+    icons: "#696969",
+    popup: "#f8f8f8",
+    succes: "#76bd70",
+    text: "#434343",
+    white: "#f8f8f8",
+  },
+  fontSize: {
+    xxl: "48px",
+    xl: "24px",
+    l: "18px",
+    m: "14px",
+    s: "12px",
+    xs: "8px",
+  },
+};
+
+export const darkTheme = {
+  colors: {
+    background: "#454545",
+    black: "#222222",
+    blue: "#0f5298",
+    board: "#565656",
+    darkPurple: "#acb3c0",
+    error: "#ce5a5a",
+    icons: "#dcdcdc",
+    popup: "#525252",
+    succes: "#76bd70",
+    text: "#d3d3d3",
+    white: "#f8f8f8",
+  },
+  fontSize: {
+    xxl: "48px",
+    xl: "24px",
+    l: "18px",
+    m: "14px",
+    s: "12px",
+    xs: "8px",
+  },
+};
