6acd5cd1e7787670cb601e044d0e4c8d28493eb8
MSzostaq <maciek.szostak55@gmail.com>
2021-11-28 17:15:15 +0100

    added light theme

---
+++ src/themes/light.js
@@ -0,0 +1,23 @@
+export default {
+  colors: {
+    background: "#f5f5f5",
+    black: "#222222",
+    blue: "#0f5298",
+    board: "#f9f9f9",
+    error: "#ce5a5a",
+    icons: "#696969",
+    popup: "#f8f8f8",
+    shadow: "#acb3c0",
+    succes: "#76bd70",
+    text: "#434343",
+    white: "#f8f8f8",
+  },
+  fontSize: {
+    xxl: "48px",
+    xl: "24px",
+    l: "18px",
+    m: "14px",
+    s: "12px",
+    xs: "8px",
+  },
+};
