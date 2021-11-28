dfe91dfc24fcea04538a6dc24371ca9462b866da
MSzostaq <maciek.szostak55@gmail.com>
2021-11-28 17:15:06 +0100

    added dark theme

---
+++ src/themes/dark.js
@@ -0,0 +1,23 @@
+export default {
+  colors: {
+    background: "#454545",
+    black: "#222222",
+    blue: "#0f5298",
+    board: "#565656",
+    error: "#ce5a5a",
+    icons: "#dcdcdc",
+    popup: "#525252",
+    shadow: "#343434",
+    succes: "#76bd70",
+    text: "#d3d3d3",
+    white: "#f8f8f8",
+  },
+  fontSize: {
+    xxl: "48px",
+    xl: "24px",
+    l: "18px",
+    m: "14px",
+    s: "12px",
+    xs: "8px",
+  },
+};
