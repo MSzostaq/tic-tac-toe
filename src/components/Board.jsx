02de74155bbd2238a2b9b3b9199b031c1edf8de6
MSzostaq <maciek.szostak55@gmail.com>
2021-11-30 18:21:01 +0100

    changed Board component

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,31 +2,21 @@ import React from "react";
 import styled from "styled-components";
 import Square from "components/Square";
 
-const Wrapper = styled.div`
+const BoardWrapper = styled.div`
   background-color: ${({ theme }) => theme.colors.board};
   border-radius: 8px;
   box-shadow: 0px 0px 2px 0px ${({ theme }) => theme.colors.darkPurple};
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   grid-template-rows: 1fr 1fr 1fr;
-  gap: 0.3rem;
-  width: 360px;
-  height: 360px;
-
-  @media (min-width: 800px) {
-    width: 420px;
-    height: 420px;
-  }
+  gap: 0.4rem;
+  padding: 8px;
+  width: 480px;
+  height: 480px;
 `;
 
-const Board = ({ squares, onClick }) => {
-  return (
-    <Wrapper>
-      {squares.map((square, i) => (
-        <Square key={i} value={square} onClick={() => onClick(i)} />
-      ))}
-    </Wrapper>
-  );
+const Board = ({ game }) => {
+  return <BoardWrapper></BoardWrapper>;
 };
 
 export default Board;
