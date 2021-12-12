bd72c4f6328705bb7603f489ead60080ca14beee
MSzostaq <maciek.szostak55@gmail.com>
2021-12-12 15:09:13 +0100

    added createBoard logic

--- src/actions/gameActions.js
+++ src/actions/gameActions.js
@@ -1,29 +1,41 @@
 import { SINGLE_PLAYER } from "constants/gameModes";
-import { coinToss } from "utils/utils";
+import { coinToss, createBoard } from "utils/utils";
 
 export const INIT_GAME = "INIT_GAME";
 export const initGame = ({ modeId = SINGLE_PLAYER }) => {
-  const startingPlayerId = coinToss() ? 1 : 2;
+  const currentPlayerId = coinToss() ? 1 : 2;
   const players = {
     1: {
       id: 1,
       isHuman: true,
-      symbol: startingPlayerId === 1 ? "X" : "O",
+      symbol: currentPlayerId === 1 ? "x" : "o",
       score: 0,
     },
     2: {
       id: 2,
       isHuman: modeId !== SINGLE_PLAYER,
-      symbol: startingPlayerId === 2 ? "X" : "O",
+      symbol: currentPlayerId === 2 ? "x" : "o",
       score: 0,
     },
   };
+
   return {
     type: INIT_GAME,
-    payload: { modeId, players, board: [[], [], []], moves: [] },
+    payload: {
+      modeId,
+      players,
+      currentPlayerId,
+      board: createBoard(),
+      moves: [],
+    },
   };
 };
 
-export const START_GAME = "START_GAME";
 export const PLAYER_MOVE = "PLAYER_MOVE";
+// pushes move to board array
+// pushes move to moves array
+// checks winning combinations
 export const END_GAME = "END_GAME";
+// pushes score to game board
+export const PLAY_AGAIN = "PLAY_AGAIN";
+// dispatch INIT_GAME with saved score
