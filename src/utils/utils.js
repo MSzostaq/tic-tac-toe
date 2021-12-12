9ee49a51a2da78407a3b6d7b4e69cdb64d2ed795
MSzostaq <maciek.szostak55@gmail.com>
2021-12-12 15:08:16 +0100

    added createBoard func

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,1 +1,13 @@
 export const coinToss = () => Math.random() < 0.5;
+
+export const createBoard = (size = 3, value = null) => {
+  const board = [];
+  for (let y = 0; y < size; y++) {
+    const row = [];
+    for (let x = 0; x < size; x++) {
+      row[x] = value;
+    }
+    board[y] = row;
+  }
+  return board;
+};
