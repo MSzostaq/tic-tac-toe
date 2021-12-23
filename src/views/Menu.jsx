d2daec603ac2be1732f81981717fe30faa33f80e
MSzostaq <maciek.szostak55@gmail.com>
2021-12-23 18:54:43 +0100

    removed About popup

--- src/views/Menu.jsx
+++ src/views/Menu.jsx
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import { useTranslation } from "react-i18next";
-import About from "popups/About";
 import Icon from "components/Icon";
-import Modal from "components/Modal";
 
 const Wrapper = styled.div`
   background-color: ${({ theme }) => theme.colors.background};
@@ -52,12 +50,6 @@ const StyledNavLink = styled(NavLink)`
 const Dashboard = () => {
   const { t } = useTranslation();
 
-  const [modal, setModal] = useState(null);
-
-  function openModal() {
-    setModal({});
-  }
-
   return (
     <Wrapper>
       <Logo icon="xoxo" />
@@ -74,12 +66,6 @@ const Dashboard = () => {
           {t("multiplayer_online")}
         </StyledNavLink>
       </Button>
-      <Button onClick={openModal}>{t("about")}</Button>
-      {modal && (
-        <Modal onClose={() => setModal(null)}>
-          <About onClose={() => setModal(null)} />
-        </Modal>
-      )}
     </Wrapper>
   );
 };
