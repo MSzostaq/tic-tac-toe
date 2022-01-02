d05cac7a6f61b19b5d693414d9bbc7d7b66f74ee
MSzostaq <maciek.szostak55@gmail.com>
2022-01-02 18:12:44 +0100

    added score and SET_PLAYER_SCORE action

--- src/actions/gameActions.js
+++ src/actions/gameActions.js
@@ -2,20 +2,20 @@ import { SINGLE_PLAYER } from "constants/gameModes";
 import { coinToss, createBoard } from "utils/utils";
 
 export const INIT_GAME = "INIT_GAME";
-export const initGame = ({ modeId = SINGLE_PLAYER }) => {
+export const initGame = ({ modeId = SINGLE_PLAYER, score }) => {
   const currentPlayerId = coinToss() ? 1 : 2;
   const players = {
     1: {
       id: 1,
       isHuman: true,
       symbol: currentPlayerId === 1 ? "x" : "o",
-      score: 0,
+      score: score ? score[1] : 0,
     },
     2: {
       id: 2,
       isHuman: modeId !== SINGLE_PLAYER,
       symbol: currentPlayerId === 2 ? "x" : "o",
-      score: 0,
+      score: score ? score[2] : 0,
     },
   };
 
@@ -32,10 +32,8 @@ export const initGame = ({ modeId = SINGLE_PLAYER }) => {
 };
 
 export const PLAYER_MOVE = "PLAYER_MOVE";
-// pushes move to board array
-// pushes move to moves array
-// checks winning combinations
 export const END_GAME = "END_GAME";
-// pushes score to game board
+export const SET_PLAYER_SCORE = "SET_PLAYER_SCORE";
 export const PLAY_AGAIN = "PLAY_AGAIN";
+// TODO:
 // dispatch INIT_GAME with saved score
