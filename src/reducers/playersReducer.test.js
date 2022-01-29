68897ddc0ebd74cd4c05f0dfe5de953e6075196d
MSzostaq <maciek.szostak55@gmail.com>
2022-01-29 20:51:16 +0100

    changed playersReducer test

--- src/reducers/playersReducer.test.js
+++ src/reducers/playersReducer.test.js
@@ -1,27 +1,123 @@
-import { INIT_GAME } from "actions/gameActions";
-import { SINGLE_PLAYER } from "constants/gameModes";
+import { INIT_GAME, PLAY_AGAIN, SET_PLAYER_SCORE } from "actions/gameActions";
 import reducer from "reducers/playersReducer";
 
 describe("players reducer", () => {
-  it("should add players to the state", () => {
+  it("should return initial state", () => {
+    expect(reducer(undefined, {})).toEqual({});
+  });
+
+  it("should set player on INIT_GAME", () => {
     const state = [];
     const action = {
       type: INIT_GAME,
       payload: {
-        modeId: SINGLE_PLAYER,
         players: {
-          1: { id: 1, isHuman: true, symbol: "X", score: 0 },
-          2: { id: 2, isHuman: false, symbol: "O", score: 0 },
+          "1": {
+            id: 1,
+            isHuman: true,
+            symbol: "x",
+            score: 0,
+          },
+          "2": {
+            id: 2,
+            isHuman: false,
+            symbol: "o",
+            score: 0,
+          },
         },
       },
     };
     const expectedState = {
-      players: {
-        1: { id: 1, isHuman: true, symbol: "X", score: 0 },
-        2: { id: 2, isHuman: false, symbol: "O", score: 0 },
+      "1": {
+        id: 1,
+        isHuman: true,
+        symbol: "x",
+        score: 0,
+      },
+      "2": {
+        id: 2,
+        isHuman: false,
+        symbol: "o",
+        score: 0,
       },
     };
+    expect(reducer(state, action)).toEqual(expectedState);
+  });
 
+  it("should set player on PLAY_AGAIN", () => {
+    const state = [];
+    const action = {
+      type: PLAY_AGAIN,
+      payload: {
+        players: {
+          "1": {
+            id: 1,
+            isHuman: true,
+            symbol: "x",
+            score: 0,
+          },
+          "2": {
+            id: 2,
+            isHuman: false,
+            symbol: "o",
+            score: 0,
+          },
+        },
+      },
+    };
+    const expectedState = {
+      "1": {
+        id: 1,
+        isHuman: true,
+        symbol: "x",
+        score: 0,
+      },
+      "2": {
+        id: 2,
+        isHuman: false,
+        symbol: "o",
+        score: 0,
+      },
+    };
+    expect(reducer(state, action)).toEqual(expectedState);
+  });
+
+  it("should set score on SET_PLAYER_SCORE", () => {
+    const state = {
+      "1": {
+        id: 1,
+        isHuman: true,
+        symbol: "x",
+        score: 0,
+      },
+      "2": {
+        id: 2,
+        isHuman: false,
+        symbol: "o",
+        score: 0,
+      },
+    };
+    const action = {
+      type: SET_PLAYER_SCORE,
+      payload: {
+        playerId: 1,
+        score: 1,
+      },
+    };
+    const expectedState = {
+      "1": {
+        id: 1,
+        isHuman: true,
+        symbol: "x",
+        score: 1,
+      },
+      "2": {
+        id: 2,
+        isHuman: false,
+        symbol: "o",
+        score: 0,
+      },
+    };
     expect(reducer(state, action)).toEqual(expectedState);
   });
 });
