7e7b0b9feba9587390ab10bbaedc45fe6939b57a
MSzostaq <maciek.szostak55@gmail.com>
2022-01-29 20:49:30 +0100

    fixed gameAction tests

--- src/actions/gameActions.test.js
+++ src/actions/gameActions.test.js
@@ -1,4 +1,9 @@
-import { initGame, INIT_GAME } from "actions/gameActions";
+import {
+  initGame,
+  INIT_GAME,
+  playAgain,
+  PLAY_AGAIN,
+} from "actions/gameActions";
 import {
   SINGLE_PLAYER,
   MULTI_PLAYER,
@@ -12,7 +17,7 @@ describe("game actions", () => {
       const { modeId, players, currentPlayerId, board, moves } = action.payload;
       const p1 = players[1];
       const p2 = players[2];
-      const xo = ["X", "O"];
+      const xo = ["x", "o"];
       expect(action.type).toBe(INIT_GAME);
       expect(modeId).toBe(SINGLE_PLAYER);
       expect(p1.id).toBe(1);
@@ -20,11 +25,11 @@ describe("game actions", () => {
       expect([1, 2].includes(currentPlayerId)).toBe(true);
       expect(p1.isHuman).toBe(true);
       expect(p2.isHuman).toBe(false);
-      expect(xo.includes(p1.symbol)).toBe(false);
-      expect(xo.includes(p2.symbol)).toBe(false);
+      expect(xo.includes(p1.symbol)).toBe(true);
+      expect(xo.includes(p2.symbol)).toBe(true);
       expect(p1.symbol).not.toBe(p2.symbol);
       expect(p1.score).toBe(0);
-      expect(p1.score).toBe(0);
+      expect(p2.score).toBe(0);
       expect(board).toEqual([
         [null, null, null],
         [null, null, null],
@@ -38,18 +43,18 @@ describe("game actions", () => {
       const { modeId, players, board, moves } = action.payload;
       const p1 = players[1];
       const p2 = players[2];
-      const xo = ["X", "O"];
+      const xo = ["x", "o"];
       expect(action.type).toBe(INIT_GAME);
       expect(modeId).toBe(MULTI_PLAYER);
       expect(p1.id).toBe(1);
       expect(p2.id).toBe(2);
       expect(p1.isHuman).toBe(true);
       expect(p2.isHuman).toBe(true);
-      expect(xo.includes(p1.symbol)).toBe(false);
-      expect(xo.includes(p2.symbol)).toBe(false);
+      expect(xo.includes(p1.symbol)).toBe(true);
+      expect(xo.includes(p2.symbol)).toBe(true);
       expect(p1.symbol).not.toBe(p2.symbol);
       expect(p1.score).toBe(0);
-      expect(p1.score).toBe(0);
+      expect(p2.score).toBe(0);
       expect(board).toEqual([
         [null, null, null],
         [null, null, null],
@@ -63,18 +68,62 @@ describe("game actions", () => {
       const { modeId, players, board, moves } = action.payload;
       const p1 = players[1];
       const p2 = players[2];
-      const xo = ["X", "O"];
+      const xo = ["x", "o"];
       expect(action.type).toBe(INIT_GAME);
       expect(modeId).toBe(MULTI_PLAYER_ONLINE);
       expect(p1.id).toBe(1);
       expect(p2.id).toBe(2);
       expect(p1.isHuman).toBe(true);
       expect(p2.isHuman).toBe(true);
-      expect(xo.includes(p1.symbol)).toBe(false);
-      expect(xo.includes(p2.symbol)).toBe(false);
+      expect(xo.includes(p1.symbol)).toBe(true);
+      expect(xo.includes(p2.symbol)).toBe(true);
       expect(p1.symbol).not.toBe(p2.symbol);
       expect(p1.score).toBe(0);
-      expect(p1.score).toBe(0);
+      expect(p2.score).toBe(0);
+      expect(board).toEqual([
+        [null, null, null],
+        [null, null, null],
+        [null, null, null],
+      ]);
+      expect(moves).toEqual([]);
+    });
+  });
+
+  describe("playAgain", () => {
+    it("should create an action to play again", () => {
+      const previousPlayers = {
+        "1": {
+          id: 1,
+          isHuman: true,
+          symbol: "x",
+          score: 1,
+        },
+        "2": {
+          id: 2,
+          isHuman: false,
+          symbol: "o",
+          score: 0,
+        },
+      };
+      const action = playAgain({
+        modeId: SINGLE_PLAYER,
+        players: previousPlayers,
+      });
+      const { modeId, players, currentPlayerId, board, moves } = action.payload;
+      const p1 = players[1];
+      const p2 = players[2];
+      expect(action.type).toBe(PLAY_AGAIN);
+      expect(modeId).toBe(SINGLE_PLAYER);
+      expect(p1.id).toBe(1);
+      expect(p2.id).toBe(2);
+      expect([1, 2].includes(currentPlayerId)).toBe(true);
+      expect(currentPlayerId).toBe(2);
+      expect(p1.isHuman).toBe(true);
+      expect(p2.isHuman).toBe(false);
+      expect(p1.symbol).toBe("o");
+      expect(p2.symbol).toBe("x");
+      expect(p1.score).toBe(1);
+      expect(p2.score).toBe(0);
       expect(board).toEqual([
         [null, null, null],
         [null, null, null],
